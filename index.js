d71f908f16ebaf202995177a22b545bee0feb6dd
Kamikazi-Deborah <deborahkamikazi9@gmail.com>
2024-07-17 11:41:16 +0200

    react plactices ex2, ex3 from scrimba

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-ReactDOM.render(<h1>Hello, everyone</h1>, document.getElementById("root"));
+// Exercise 1
+
+const page = (
+     <div>
+    <h1>here is the sum of the number {5+5}</h1>
+     <h2> welcome</h2>
+     </div>)
+     console.log(page)
+ReactDOM.render( page,document.getElementById("root"));
+// Exercise 2 using {} to add expression in JSX
+
+ const root = ReactDOM.createRoot(document.getElementById("root"))
+ root.render(<h1> here is the sum {5+6} </h1>)
+
+
+  // Exercise 3 Create a navbar in JSX:
+    // - Use the semantic `nav` element as the parent wrapper
+    // - Have an h1 element with the brand name of your "website"
+    // - Insert an unordered list for the other nav elements
+    //     - Inside the `ul`, have three `li`s for "Pricing",
+    //     "About", and "Contact"
+const navs = (
+    <nav>
+      <h1>website</h1>
+      <ul>
+        <li>Pricing</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+)
+
+ReactDOM.render(navs,document.getElementById('root'))
